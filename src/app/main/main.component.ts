2a629cbb117b3a819db3ce895e0052045c977d41
Mikita Dubovik <Mikita_Dubovik@epam.com>
2020-01-21 15:49:48 +0300

    Added CreatedByMe

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,20 +9,28 @@ import { Article } from '../models/article';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
   @Input() isAdded: boolean;
+  createdByMe: boolean;
   sources: Source[];
   sourceId: string;
   articles: Article[];
+  originalArticles: Article[];
   title: string;
   articlePage: number;
   filterInput: string;
 
+  //Const
+  myTitle: string;
+  defaulTitle: string;
 
   constructor(private apiService: ApiService) {
+    this.myTitle = 'AMASING NEWS';
+    this.defaulTitle = 'Please, choose source'
   }
 
   ngOnInit() {
-    this.title = 'Please, choose source';
+    this.title = this.defaulTitle;
     this.apiService.getSources().subscribe(
       resp => {
         this.sources = resp;
@@ -30,46 +38,81 @@ export class MainComponent implements OnInit {
     );
   }
 
-  onChangeObj(selectedSource) {
+  onChangeSource(selectedSource) {
     this.sourceId = selectedSource.value
     this.apiService.getArticles(this.sourceId, 1).subscribe(
       resp => {
         if (resp.length > 0) {
-          this.articles = resp;
+          this.originalArticles = resp;
+          this.articles = this.originalArticles;
           this.articlePage = 1;
           this.isAdded = true;
-          this.filterInput = "";
+          this.filterInput = '';
+          this.createdByMe = false;
         } else {
           alert('NEWS API IS BROKEN');
         }
       }
     );
 
-    this.title = this.sources.find(s => s.id === this.sourceId).name;
+    this.setSourceTitle();
   }
 
   loadMore() {
     this.articlePage++;
-    this.apiService.getArticles(this.sourceId, this.articlePage).subscribe(
-      resp => {
-        if (resp.length > 0) {
-          if (this.filterInput) {
-            if (resp.find(art => art.title.includes(this.filterInput))) {
-              this.articles.push(...resp.filter(art => art.title.includes(this.filterInput)));
-            } else {
-              this.isAdded = false;
+
+    //For future task with own DB
+    //Add check for Load More button
+    if (this.createdByMe) {
+      this.createdByMeFilter(true);
+      //this.originalArticles = this.nodeService.getArticles(this.articlePage);
+    } else {
+      this.apiService.getArticles(this.sourceId, this.articlePage).subscribe(
+        resp => {
+          if (resp.length > 0) {
+            this.originalArticles.push(...resp) //added for all cases
+
+            if (this.filterInput) {
+              if (resp.find(r => r.title.includes(this.filterInput))) {
+                this.articles.push(...resp.filter(art => art.title.includes(this.filterInput)));
+              } else {
+                this.isAdded = false;
+              }
+            } else { //if no filterInput
+              this.articles = this.originalArticles;
             }
           } else {
-            this.articles.push(...resp);
+            this.isAdded = false;
           }
-        } else {
-          this.isAdded = false;
         }
-      }
-    );
+      );
+    }
   }
 
-  filter() {
-    this.articles = this.articles.filter(art => art.title.includes(this.filterInput));
+  globalFilter() {
+    if (this.filterInput) {
+      this.articles = this.articles.filter(art => art.title.includes(this.filterInput));
+    } else {
+      this.articles = this.originalArticles;
+    }
+  }
+
+  createdByMeFilter(selectedOption) {
+    if (selectedOption) {
+      this.title = this.myTitle;
+      this.articles = this.articles ? this.articles.filter(art => art.createdByMe) : null;
+    }
+    else {
+      this.setSourceTitle();
+      this.globalFilter();
+    }
+  }
+
+  setSourceTitle() {
+    if (this.sourceId) {
+      this.title = this.sources.find(s => s.id === this.sourceId).name;
+    } else {
+      this.title = this.defaulTitle;
+    }
   }
 }
